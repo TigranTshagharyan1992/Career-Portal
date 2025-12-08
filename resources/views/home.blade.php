201d756c82b4fedc2b5a7bcddda3fd9f1f3e2de7
Tigran Tshagharyan <tshagharyan.tigran@gmail.com>
2025-12-08 17:06:04 +0400

    contact us funcitonality finish

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -75,23 +75,25 @@
         </section>
     </div>
     <!-- Success Modal -->
-    <!-- Success Modal -->
-    <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content rounded-4 shadow-lg">
-                <div class="modal-header bg-success text-white border-0 rounded-top">
+    <div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="successModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content rounded-lg shadow">
+                <div class="modal-header bg-success text-white">
                     <h5 class="modal-title" id="successModalLabel">Success!</h5>
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" style="color:white;">&times;</span>
+                    </button>
                 </div>
-                <div class="modal-body py-4">
-                    <p id="successMessage" class="text-center mb-0">Your message has been sent successfully.</p>
+                <div class="modal-body">
+                    <p id="successMessage">Your message has been sent successfully.</p>
                 </div>
-                <div class="modal-footer border-0 justify-content-center pb-4">
-                    <button type="button" class="btn btn-success px-4" data-bs-dismiss="modal">OK</button>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-success btn-sm" data-dismiss="modal">OK</button>
                 </div>
             </div>
         </div>
     </div>
+
 
 @endsection
 @push('page-js')
